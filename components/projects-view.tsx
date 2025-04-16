c402bb8c653d8f78aa4370103383d93e956aaae4
santhosanthosh4444 <santhosanthosh4444@gmail.com>
2025-04-16 10:57:20 +0530

    :

--- components/projects-view.tsx
+++ components/projects-view.tsx
@@ -106,7 +106,7 @@ export function ProjectsView({ user }: ProjectsViewProps) {
       const endpoint = approvalType === "mentor" ? "/api/projects/approve-mentor" : "/api/projects/approve-hod"
 
       const response = await fetch(endpoint, {
-        method: "GET",
+        method: "PUT",
         headers: {
           "Content-Type": "application/json",
         },
